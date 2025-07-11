aa618ed86ca470c50a0e8822da02f5959ea160e3
Taeyoon <taeyoonkim1995@gmail.com>
2025-07-11 16:06:22 -0700

    add Dockerfile

--- sharavel-be/src/test/java/com/sharavel/sharavel_be/SharavelBeApplicationTests.java
+++ sharavel-be/src/test/java/com/sharavel/sharavel_be/SharavelBeApplicationTests.java
@@ -2,8 +2,10 @@ package com.sharavel.sharavel_be;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SharavelBeApplicationTests {
 
 	@Test
